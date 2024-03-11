3e77fb047ca7dd2b971a3f2a51de2175f135d533
azat44 <azat4beats@gmail.com>
2024-03-11 18:19:42 +0100

    test du portfolio

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://fr.linkedin.com/in/azat-harutyunyan-487570210?trk=public_post_reshare-text'
+      className='link footer__link'
+    >
+      Created By Azat Harutyunyan
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,46 @@
+
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: '/home',
+  title: 'AH.',
+}
+
+const about = {
+  name: 'AZAT HARUTYUNYAN',
+  role: 'Junior front-end developer',
+  description:
+    'Junior front-end developer living in france ready to help with your projects',
+  resume: 'a',
+  social: {
+    linkedin: 'https://fr.linkedin.com/in/azat-harutyunyan-487570210?trk=public_post_feed-actor-name',
+    github: 'https://github.com/azat4444',
+  },
+}
+
+const projects = [
+  {
+    name: 'Arev Society',
+    description:
+      'The Arev Society fosters and encourages philanthropy and solidarity of American people in support of vulnerable Armenians worldwide by supporting and financing initiatives for',
+    livePreview: 'https://arevsociety.org/index.html',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'React-Native',
+  'Git',
+  'After-Effect',
+  'Premiere Pro',
+
+]
+
+const contact = {
+
+  email: 'azat.harut@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
